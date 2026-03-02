c5524c307c61da94c7e9b49b36eac7dab7ccc740
Ddude-02 <dummuydude002@gmail.com>
2026-03-02 23:23:07 +0530

    printing elements in array

---
+++ PrintArr.java
@@ -0,0 +1,9 @@
+public class PrintArr {
+    public static void printArray(int[] arr) {
+        System.out.println("the elements of the array are:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
