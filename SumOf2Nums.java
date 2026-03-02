7fa34c091692995f884f28765181e9da131451d3
Ddude-02 <dummuydude002@gmail.com>
2026-03-02 21:16:32 +0530

    a+b prob

---
+++ SumOf2Nums.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class SumOf2Nums{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the 2 nums:");
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        System.out.println("sum of 2 giving numbers is:"+(n1+n2));
+    }
+}
